c2a599634284c849e931a4eb6ec3ae64154c4b02
Taras Glek <tglek@mozilla.com>
2011-06-13 16:59:08 -0700

    imported patch foo.diff

--- bogroll.ml
+++ bogroll.ml
@@ -9,20 +9,20 @@ open Http_client.Convenience
 open Xml
 
 let get_PCData = function 
-  | Xml.Element(_,_,[Xml.PCData str]) -> str 
+  | Element(_,_,[Xml.PCData str]) -> str 
   | _ -> raise Not_found
 
 let get_subelement ls name =
-  List.find (function Xml.Element (ename, _, _) -> ename = name | _ -> false) ls
+  List.find (function Element (ename, _, _) -> ename = name | _ -> false) ls
   
 let get_subelement_PCData ls name =
   get_PCData (get_subelement ls name)
 
 let filter_items ls name = 
-  List.filter (function Xml.Element(ename, _, _) -> ename = name | _ -> false) ls
+  List.filter (function Element(ename, _, _) -> ename = name | _ -> false) ls
    
 let reduce_item = function 
-  | Xml.Element("entry",_, ls) -> 
+  | Element("entry",_, ls) -> 
       let title = get_subelement_PCData ls "title" in
       let content = get_subelement_PCData ls "content" in
       (title,content)
@@ -30,7 +30,7 @@ let reduce_item = function
 
 (* Returns the ls of (title, content) *)
 let get_atom_ls = function 
-  | Xml.Element("feed", _, ls) ->
+  | Element("feed", _, ls) ->
       let title = get_PCData (get_subelement ls "title") in
       let ls = filter_items ls "entry" in
       let ls = List.map reduce_item ls in
@@ -51,7 +51,7 @@ let read f =
 let rec maybe_raw_xml text =
   try 
     Xml.parse_string ("<p>" ^ text ^ "</p>")
-  with Xml.Error (Xml.EndOfTagExpected tag as msg, pos)->
+  with Xml.Error (Xml.EndOfTagExpected tag, pos)->
     let (a, b) = Xml.abs_range pos in
     let len = String.length tag in
     let rec reverse_scan i =
@@ -65,25 +65,29 @@ let rec maybe_raw_xml text =
     let text = String.sub text 0 tokend ^ "</" ^ tag ^ ">" ^ String.sub text tokend (String.length text - tokend) in
     maybe_raw_xml text
 (*      print_string ((String.sub text start (tokend - start+len*4))^"\n");
-    Xml.Element ("p", [], [Xml.PCData text]) *)
+    Element ("p", [], [Xml.PCData text]) *)
 
 let chaperify ls =
-  let ret = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+  let head = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <!DOCTYPE html
   PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
-<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">
-   <head>
-      <title>CHAPTER I | Pierre and Jean</title>
-      <meta http-equiv=\"Content-Type\" content=\"application/xhtml+xml; charset=utf-8\"/>
-      <meta name=\"EPB-UUID\" content=\"753C90B5-6C08-1014-A044-DC40CEEA30C3\"/>
-   </head>
-
 " in
-  let tail = " </html>" in
-  let ls = List.map (function (title, body) -> [Xml.Element( "h3", [], [Xml.PCData title]); 
+  let ls = List.map (function (title, body) -> [Element( "h3", [], [Xml.PCData title]); 
                                                 maybe_raw_xml body]) ls in
-  let body = Xml.to_string_fmt(Xml.Element("body", [], List.flatten ls)) in
-    ret ^ body ^ tail
+
+
+
+  let e = 
+    Element ("html", [("xmlns", "http://www.w3.org/1999/xhtml"); ("xml:lang", "en")], [
+               Element ("head", [], [
+                          Element ("title", [], [Xml.PCData "some title"]);
+                          Element ("meta", ["http-equiv","Content-Type";"content","application/xhtml+xml; charset=utf-8"], []);
+                          Element ("meta", ["name","EPB-UUID"; "content","753C90B5-6C08-1014-A044-DC40CEEA30C3"], [])
+                        ]);
+               Element ("body", [], List.flatten ls)]
+            ) in
+  let body = Xml.to_string_fmt e in
+    head ^ body
     
 
 let write_file dir file content =
