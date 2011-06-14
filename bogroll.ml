0759ea8a91d55f78b432c20d5ed4034539d6fbbe
Taras Glek <tglek@mozilla.com>
2011-06-13 17:54:46 -0700

    broke stuff up into chapters

--- bogroll.ml
+++ bogroll.ml
@@ -28,7 +28,7 @@ let reduce_item = function
       (title,content)
   | _ -> failwith "oops"
 
-(* Returns the ls of (title, content) *)
+(* Returns  (title, [title*content]) *)
 let get_atom_ls = function 
   | Element("feed", _, ls) ->
       let title = get_PCData (get_subelement ls "title") in
@@ -67,27 +67,30 @@ let rec maybe_raw_xml text =
 (*      print_string ((String.sub text start (tokend - start+len*4))^"\n");
     Element ("p", [], [Xml.PCData text]) *)
 
-let chaperify ls =
+let counter = ref 1
+
+let new_chapter_id () = 
+  let ret = !counter in
+    counter := !counter + 1;
+    "chapter" ^ (string_of_int ret) ^ ".xhtml"
+  
+let chaperify (title, body) =
   let head = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <!DOCTYPE html
   PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
 " in
-  let ls = List.map (function (title, body) -> [Element( "h3", [], [Xml.PCData title]); 
-                                                maybe_raw_xml body]) ls in
-
-
-
+  let id = new_chapter_id () in
+  let ls = [Element( "h3", [], [Xml.PCData title]); maybe_raw_xml body] in
   let e = 
     Element ("html", [("xmlns", "http://www.w3.org/1999/xhtml"); ("xml:lang", "en")], [
                Element ("head", [], [
-                          Element ("title", [], [Xml.PCData "some title"]);
+                          Element ("title", [], [Xml.PCData title]);
                           Element ("meta", ["http-equiv","Content-Type";"content","application/xhtml+xml; charset=utf-8"], []);
-                          Element ("meta", ["name","EPB-UUID"; "content","753C90B5-6C08-1014-A044-DC40CEEA30C3"], [])
                         ]);
-               Element ("body", [], List.flatten ls)]
+               Element ("body", [], ls)]
             ) in
   let body = Xml.to_string_fmt e in
-    head ^ body
+    (id, head ^ body)
     
 
 let write_file dir file content =
@@ -96,7 +99,8 @@ let write_file dir file content =
     output_string out content;
     close_out out
 
-let opf title =
+let opf title filenames =
+  let head =
 "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 
 <package xmlns=\"http://www.idpf.org/2007/opf\" unique-identifier=\"EPB-UUID\" version=\"2.0\">
@@ -108,18 +112,21 @@ let opf title =
       <dc:creator opf:role=\"aut\" opf:file-as=\"Maupassant, Guy De\">Guy De Maupassant</dc:creator>
       <dc:publisher>epubBooks (www.epubbooks.com)</dc:publisher>
       <dc:creator opf:file-as=\"Austen, Jane\" opf:role=\"aut\">Jane Austen</dc:creator>
-   </metadata>
-   <manifest>
-    <item id=\"content\" href=\"content.xhtml\" media-type=\"application/xhtml+xml\"/>
-    <item id=\"ncx\" href=\"toc.ncx\" media-type=\"application/x-dtbncx+xml\"/>
-   </manifest>
-   <spine toc=\"ncx\">
-      <itemref idref=\"content\" linear=\"yes\"/>
-   </spine>
+   </metadata>"
+  in
+  let ls = List.map (fun name -> Element ("item", ["id",name;"href",name;"media-type","application/xhtml+xml"],[])) filenames in
+  let e = Element ("manifest", [], 
+                   (Element ("item", ["id","ncx";"href","toc.ncx";"media-type","application/x-dtbncx+xml"],[]))::ls
+                   ) in
+  let spinels = List.map (fun name -> Element("itemref", ["idref",name;"linear","yes"], [])) filenames in
+  let spine = Element ("spine", ["toc","ncx"], spinels) in
+   let tail = (Xml.to_string_fmt spine) ^ "
 </package>
-"
+" in
+     head ^ (Xml.to_string_fmt e) ^ tail
 
-let toc title =
+let toc title filenames =
+  let head = 
 "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <!DOCTYPE ncx
   PUBLIC \"-//NISO//DTD ncx 2005-1//EN\" \"http://www.daisy.org/z3986/2005/ncx-2005-1.dtd\">
@@ -135,7 +142,8 @@ let toc title =
    </docTitle>
   <docAuthor>
     <text>Austen, Jane</text>
-  </docAuthor>
+  </docAuthor>" in
+  let _ = "
      <navMap>
       <navPoint id=\"content\" playOrder=\"1\">
          <navLabel>
@@ -143,8 +151,23 @@ let toc title =
          </navLabel>
          <content src=\"content.xhtml\"/>
       </navPoint>
-   </navMap>
-</ncx>"
+   </navMap>" in
+  let tail = "</ncx>" in
+  let counter = ref 0 in
+  let new_counter () =
+    counter := !counter + 1;
+    string_of_int !counter
+  in
+  let navpoint name = 
+    Element ("navPoint", ["id","content"; "playOrder",new_counter()], [
+               Element ("navLabel", [], [
+                          Element ("text", [], [PCData name])
+                        ]);
+               Element ("content", ["src",name], [])
+             ])
+  in
+  let e = Element("navMap", [], List.map navpoint filenames) in
+  head ^ (Xml.to_string_fmt e) ^ tail
 
 let container =
 "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
@@ -163,11 +186,13 @@ let main infile outdir =
       http_get infile 
   in
   let (title, items) = get_atom_ls (Xml.parse_string body) in
+  let items = List.map (chaperify) items in
+  let file_names = fst (List.split items) in
     write_file outdir "mimetype" "application/epub+zip";
     write_file (outdir ^ "/META-INF") "container.xml" container;
-    write_file (outdir ^ "/OPS") "content.xhtml" (chaperify items);
-    write_file (outdir ^ "/OPS") "content.opf"  (opf title);
-    write_file (outdir ^ "/OPS") "toc.ncx"  (toc title)
+    List.iter (fun (name, content) -> write_file (outdir ^ "/OPS") name  content) items;
+    write_file (outdir ^ "/OPS") "content.opf"  (opf title file_names);
+    write_file (outdir ^ "/OPS") "toc.ncx"  (toc title file_names)
 ;;
 
 main Sys.argv.(1) Sys.argv.(2)
